5bf26c3eab57f88ecc7d43a381024e7e4e24e874
Robert Perez <robertjasonperez@gmail.com>
2015-07-21 00:02:15 -0500

    Message length data frame

--- R Analysis/GroupMe.R
+++ R Analysis/GroupMe.R
@@ -1,31 +1,43 @@
-readInFile <- function(){
+readInFile <- function() {
     json <- fromJSON("54790.json")
     messageList <- json$response$messages
     messageList
 }
 
 
-dateTimeCount <- function(createdAtPosixlt){
+dateTimeCount <- function(createdAtPosixlt) {
     hours <- 0:23
-    hoursCount <- vector(mode="numeric", length = 24)
-    for(date in createdAtPosixlt){
+    hoursCount <- vector(mode = "numeric", length = 24)
+    for (date in createdAtPosixlt) {
         posixlt <- as.POSIXlt(date,origin = "1970-1-1")
-        hoursCount[posixlt$hour+1] <-hoursCount[posixlt$hour+1]+1
-
+        hoursCount[posixlt$hour + 1] <-
+            hoursCount[posixlt$hour + 1] + 1
+        
     }
     data.frame(Hour = hours,MessageCount = hoursCount)
 }
 
-dateTimeCountByUser <- function(messages){
-    users <- unique(msg$sender_id[msg$sender_id!="system"])
+dateTimeCountByUser <- function(messages) {
+    users <- unique(msg$sender_id[msg$sender_id != "system"])
     hourCounts <- vector(mode = "list",length = length(users))
-    for(i in 1:length(users)){
-        hourCounts[[i]] <- dateTimeCount(msg$created_at[msg$sender_id==users[i]])
+    for (i in 1:length(users)) {
+        hourCounts[[i]] <-
+            dateTimeCount(msg$created_at[msg$sender_id == users[i]])
     }
     hourCounts
 }
 
-plotGroup<- function(dfs){
+plotGroup <- function(dfs) {
     colors <- rainbow(1:length(dfs))
     plot()
+}
+
+messageLengths <- function(messageStrings) {
+    maxLength <- max(nchar(messageStrings))
+    nchars <- vector(mode = "numeric",length = maxLength)
+    for (index in 1:length(messageStrings)) {
+        nchars[nchar(messageStrings[[index]])] <-
+            (nchars[nchar(messageStrings[[index]])] + 1)
+    }
+    data.frame(1:100,nchars[1:100])
 }
